e37a51c279f4606d6dc8036fcd01823194964cb2
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2023-10-25 18:13:56 +0530

    Numeron2

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+let play_Btn = document.getElementById("play-button");
+console.log(play_Btn)
+
+play_Btn.addEventListener("click", redirect)
+
+function redirect(){
+    location.href = "./game.html"
+}
